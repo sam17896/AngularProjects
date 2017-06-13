f14c20098f15111e5a59f64ac92b76eed6d26b24
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-13 19:04:43 +0500

    Angular Website: Navbar

--- angular-website/src/app/app.module.ts
+++ angular-website/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavbarComponent
   ],
   imports: [
     BrowserModule
