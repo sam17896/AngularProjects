b34f7f099c8515356405cd898f34cbdefe77c73f
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-20 02:35:30 +0500

    MeanTODO : Todo Component UI

--- MeanTODO/client/src/app/app.module.ts
+++ MeanTODO/client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos.component';
+import { NavbarComponent } from './components/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TodosComponent, NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ MeanTODO/client/src/app/components/todos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId : module.id,
+  selector: 'todos',
+  templateUrl: 'todos.component.html',
+})
+export class TodosComponent {
+  
+}
