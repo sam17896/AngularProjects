c467b08444600f1c0b8613d2d92d05a145407bb0
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-20 01:31:56 +0500

    MeanTODO : Client Setup

--- MeanTODO/server.js
+++ MeanTODO/server.js
@@ -11,6 +11,9 @@ var app = express();
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname,'client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
