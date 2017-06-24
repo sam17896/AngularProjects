42909916e959819066c9cd44e4c9905f919a543e
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-24 15:50:26 +0500

    My Weather : Tabs

--- myweather/src/pages/setting/setting.ts
+++ myweather/src/pages/setting/setting.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'setting.html'
 })
-export class ContactPage {
+export class SettingPage {
 
   constructor(public navCtrl: NavController) {
 
--- myweather/src/pages/tabs/tabs.ts
+++ myweather/src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { SettingPage } from '../setting/setting';
 import { HomePage } from '../home/home';
 
 @Component({
@@ -10,8 +9,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = SettingPage;
 
   constructor() {
 
