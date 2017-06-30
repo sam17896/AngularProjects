14f6fc347cb825e18b2e553fe558860ac35e1e8c
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-01 03:33:16 +0500

    MapIt : Display Markers

--- mapit/src/app/app.component.ts
+++ mapit/src/app/app.component.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  //Start Position
+  lat: number = 42.858217;
+  lng: number = -70.929990;
+
+  //Zoom Level 
+  zoom : number = 10;
+
+  markers : marker[] = [
+    {
+      name : 'Company One',
+      lat : 42.825588,
+      lng : -71.018029,
+      draggable : true
+    },
+    {
+      name : 'Company Two',
+      lat : 42.868164,
+      lng : -70.889071,
+      draggable : true
+    },
+    {
+      name : 'Company Three',
+      lat : 42.858279,
+      lng : -70.930498,
+      draggable : false
+    }
+  ]
+}
+
+interface marker {
+  name? : string;
+  lat : number;
+  lng : number;
+  draggable : boolean
 }
